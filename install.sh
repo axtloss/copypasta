31dfa9da104a0b83e524f8b747a4efec376652b9
Matt C <mcompton2002@gmail.com>
2021-05-06 09:57:39 -0400

    Install xdotool

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+chmod +x copypasta.sh
 cp copypasta.sh $HOME/.local/bin/copypasta
 cp copypasta.desktop $HOME/.local/share/applications/
+sudo pacman -Sy xdotool
 echo "Installation done! Go spam your friends with the linux copypasta!"
