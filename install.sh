5787f34671686aea28d5aae86c41a0617b2d57a0
axtloss <3alouchi2006@gmail.com>
2021-05-06 16:08:28 +0200

    added freebsd xdotool install support

--- install.sh
+++ install.sh
@@ -3,8 +3,19 @@ chmod +x copypasta.sh
 cp copypasta.sh $HOME/.local/bin/copypasta
 cp copypasta.desktop $HOME/.local/share/applications/
 
+if command -v sudo &> /dev/null
+then
+    rootCommand="sudo"
+elif command -v doas &> /dev/null
+then
+    rootCommand="doas"
+fi
+
 grep -q "Arch" /etc/issue
 if [[ "$?" == "0" ]]; then
-    sudo pacman -Sy xdotool
+    $rootCommand pacman -Sy xdotool
+elif [[ $(uname) == "FreeBSD" ]]
+then
+    $rootCommand pkg install xdotool
 fi
 echo "Installation done! Go spam your friends with the linux copypasta!"
