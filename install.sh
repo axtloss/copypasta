9695df7519c99abdfe77431887a9d0ee78df8083
Matt Compton <matt@xhec.us>
2021-05-06 09:59:33 -0400

    Rename license and conditional for pacman

--- install.sh
+++ install.sh
@@ -2,5 +2,9 @@
 chmod +x copypasta.sh
 cp copypasta.sh $HOME/.local/bin/copypasta
 cp copypasta.desktop $HOME/.local/share/applications/
-sudo pacman -Sy xdotool
+
+grep -q "Arch" /etc/issue
+if [[ "$?" == "0" ]]; then
+    sudo pacman -Sy xdotool
+fi
 echo "Installation done! Go spam your friends with the linux copypasta!"
