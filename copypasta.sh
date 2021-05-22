f5c9ad24524367766b1ec21956ba4057949a2698
axtloss <3alouchi2006@gmail.com>
2021-05-22 23:23:52 +0200

    added option to choose your own term to be gnu'd

--- copypasta.sh
+++ copypasta.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
-xdotool type "I'd just like to interject for a moment. What you're refering to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.
 
-Many computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called Linux, and many of its users are not aware that it is basically the GNU system, developed by the GNU Project.
+if [[ $1 == "" ]];
+then
+    copy="linux"
+else
+    copy=$1
+fi
+xdotool type "I'd just like to interject for a moment. What you're refering to as $copy, is in fact, GNU/$copy, or as I've recently taken to calling it, GNU plus $copy. $copy is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.
 
-There really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine's resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called Linux distributions are really distributions of GNU/Linux"
+Many computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called $copy, and many of its users are not aware that it is basically the GNU system, developed by the GNU Project.
+
+There really is a $copy, and these people are using it, but it is just a part of the system they use. $copy is the kernel: the program in the system that allocates the machine's resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. $copy is normally used in combination with the GNU operating system: the whole system is basically GNU with $copy added, or GNU/$copy. All the so-called $copy distributions are really distributions of GNU/$copy"
 xdotool key Return
 xdotool type "This message was sent with: https://github.com/axtloss/copypasta"
 xdotool key Return
